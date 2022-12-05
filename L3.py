9532c62b2a940b7f2790baf46954570b1b9659fd
IsaGraceGuthrie <116840075+IsaGraceGuthrie@users.noreply.github.com>
2022-12-04 20:42:48 -0500

    Add files via upload

---
+++ L3.py
@@ -0,0 +1,11 @@
+# the authors' names are Isa Grace Guthrie ad Ellen Kevin
+import math
+x=(math.pi)/4
+y=(math.pi)/3
+a=math.tan(x)
+b=math.tan(y)
+c=math.sin(y)
+d=a+1
+e=b+c
+value=math.sqrt(d)/math.sqrt(e)
+print("value =", value)
---
+++ L4.py
@@ -0,0 +1,11 @@
+# the authors names are: Isa Grace Guthrie and Ellen Kevin
+import math
+r=4
+c=4**2*math.pi
+print("c=", c)
+l=math.sqrt(32)
+w=math.sqrt(32)
+s=l*w
+print("s=", s)
+a=c-s
+print("the area enclosed between the square and circle", a)
